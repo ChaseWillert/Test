bf0d9bff5a925aa5ff58b20cd47e7688b5e982a5
Noah <noah.heikes@knights.ucf.edu>
2021-02-11 19:50:41 +0000

    Got login and register working - Patrick

--- js/code.js
+++ js/code.js
@@ -66,20 +66,16 @@ function doRegister()
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function()
-		{
-			if (this.readyState == 4 && this.status == 200)
-			{
+	try {
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
 				window.location.href = "index.html";
 			}
 		};
 
 		xhr.send(jsonPayload);
 	}
-	catch(err)
-	{
+	catch(err) {
 		alert(err.message);
 	}
 }
